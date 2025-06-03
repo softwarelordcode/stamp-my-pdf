ee02624c10b660d13841e6680960b9e85a08b7d0
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-06-03 22:38:18 +0200

    add watermark

--- stamp_my_pdf.py
+++ stamp_my_pdf.py
@@ -5,6 +5,7 @@ Usage: python stamp_my_pdf.py <pdf_file> <pdf_watermark_file>
 
 import sys
 import os
+from pypdf import PdfReader, PdfWriter
 
 try:
     pdf_file = sys.argv[1]
@@ -20,3 +21,12 @@ if not (os.path.exists(pdf_file) and os.path.exists(watermark_file)):
 if not (pdf_file.endswith('.pdf') and watermark_file.endswith('.pdf')):
     print("Both files must be PDF files.")
     sys.exit(1)
+
+stamp = PdfReader(watermark_file).pages[0]
+writer = PdfWriter(clone_from=pdf_file)
+
+for page in writer.pages:
+    page.merge_page(stamp, over=False)
+
+writer.write(pdf_file.replace('.pdf', '_stamped.pdf'))
+print(f"Watermark applied. Stamped PDF saved as {pdf_file.replace('.pdf', '_stamped.pdf')}")
