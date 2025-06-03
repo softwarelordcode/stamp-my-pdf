9b54bbda7e3b299f80154aa5fe31e9960157112f
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-06-03 22:32:29 +0200

    read files

---
+++ stamp_my_pdf.py
@@ -0,0 +1,22 @@
+'''
+This script put a watermark in all pdf pages.
+Usage: python stamp_my_pdf.py <pdf_file> <pdf_watermark_file>
+'''
+
+import sys
+import os
+
+try:
+    pdf_file = sys.argv[1]
+    watermark_file = sys.argv[2]
+except IndexError:
+    print("Usage: python stamp_my_pdf.py <pdf_file> <pdf_watermark_file>")
+    sys.exit(1)
+
+if not (os.path.exists(pdf_file) and os.path.exists(watermark_file)):
+    print("pdf file or watermark file does not exist.")
+    sys.exit(1)
+
+if not (pdf_file.endswith('.pdf') and watermark_file.endswith('.pdf')):
+    print("Both files must be PDF files.")
+    sys.exit(1)
